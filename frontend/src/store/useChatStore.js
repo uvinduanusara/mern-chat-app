c0fdef9394d6d1f2d202dd709992fc6abb80b538
Uvindu28 <uvindu03@gmail.com>
2024-11-28 00:54:11 +0530

    frontend first

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import {axiosInstance} from '../lib/axios';
+
+export const useChatStore = create((set) => ({
+    messages: [],
+    users: [],
+    selectedUser: null,
+    isUserLoading: false,
+    isMessagesLoading: false,
+
+    getUsers: async() => {
+        set({isUserLoading: true});
+        try {
+            const res = await axiosInstance.get("/messages/users");
+            set({users: res.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }finally{
+            set({isUserLoading: false});
+        }
+    },
+
+    getMessages: async(userId) => {
+        set({isMessagesLoading: true});
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({message: res.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+            
+        }finally{set({isMessagesLoading: false})}
+    }
+}))
