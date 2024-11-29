e890ca5d291e4349f4cb73c7b285fb1c47645b9c
Uvindu28 <uvindu03@gmail.com>
2024-11-29 17:28:10 +0530

    finish commit ✨🙌😎

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from "./components/Navbar"
 import HomePage from "./pages/HomePage"
 import SignUpPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
-import SettingsPage from "./pages/SettingsPage"
 import ProfilePage from "./pages/ProfilePage"
 import { useAuthStore } from "./store/useAuthStore"
 import { useEffect } from "react"
@@ -35,7 +34,6 @@ const App = () => {
         <Route path="/" element={authUser ? <HomePage/> : <Navigate to="/login"/>}/>
         <Route path="/signup" element={!authUser ? <SignUpPage/> : <Navigate to="/"/>}/>
         <Route path="/login" element={!authUser ? <LoginPage/> : <Navigate to="/"/>}/>
-        <Route path="/settings" element={<SettingsPage/>}/>
         <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
       </Routes>
       <Toaster/>
