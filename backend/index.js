ae6e721937c6a57391b8128eaa562f6008f5ea16
Uvindu28 <uvindu03@gmail.com>
2024-11-30 20:49:46 +0530

    updated

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
+if(process.env.FRONTEND_URL==="production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req,res) => {
