9f3669ae92f9ffae225c0bea87d1e5beb7eb1a32
Uvindu28 <uvindu03@gmail.com>
2024-11-30 21:29:44 +0530

    updated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://whatz-server.vercel.app/?vercelToolbarCode=sDq9q8gHpj5lhYa"; 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/"; 
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
