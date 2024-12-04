cfb9e754ac8b26221adb32740484fb6f71c4000d
Uvindu28 <uvindu03@gmail.com>
2024-12-04 10:42:35 +0530

    updated

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoutes from "./src/routes/auth.route.js";
-import messageRoutes from "./src/routes/message.route.js";
-import { connectDB } from './src/lib/db.js';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser'; 
 import cors from 'cors';
-import { app, server } from './src/lib/socket.js';
+import { app, server } from './lib/socket.js';
 import path from 'path';
 dotenv.config()
 
